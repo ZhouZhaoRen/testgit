44d7454c0b3df664412e8c5864ce4c9bb9524bce
ZhouZhaoRen <762513499@qq.com>
2020-07-27 17:26:15 +0800

    再一次测试

--- Java-Test02/src/main/java/com/atomic/TestNewFile.java
+++ Java-Test02/src/main/java/com/atomic/TestNewFile.java
@@ -4,5 +4,7 @@ public class TestNewFile {
 	public static void main(String[] args) {
 		System.out.println("这是用来测试新建文件上传到Git");
 		System.out.println("再次测试");
+		long a = 222222222;
+		System.out.println(a);
 	}
 }
